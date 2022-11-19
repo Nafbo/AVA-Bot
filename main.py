fb0a83be4b0fa3b64452299809fcf9dc6e4e62bf
Nafbo <victor.bonnaf@gmail.com>
2022-11-19 17:57:39 +0100

    .

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ warnings.filterwarnings('ignore')
 def main():
     analysis = Analysis()
     pairList = ['BTC/USDT', 'ETH/USDT', 'BNB/USDT', 'XRP/USDT', 'ADA/USDT']
-    return(analysis.analyzeBacktest(pairList, '1h',usd = 100, start_date='2022-01-01'))
+    return(analysis.analyzeBacktest(pairList, '1h',usd = 100, start_date='2022-01-02'))
 
 if __name__ == '__main__':
     print(main())

9c398cb84cc145d9d80c8e1042952d1305d8c841
Nafbo <victor.bonnaf@gmail.com>
2023-02-28 22:48:00 +0100

    y

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ sudo apt-get update
 echo Installation de pip...
 sudo apt install pip -y
 echo Installation des packages nécessaires...
-pip install -r requirements.txt
+pip install -r AVA-Bot/requirements.txt
 touch cronlog.log
 
 echo Installation finie vous pouvez maintenant commencer par modifier le fichier secret.json en ajoutant un sous compte avec ses clés api.

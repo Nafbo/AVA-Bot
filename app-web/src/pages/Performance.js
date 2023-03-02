a37bb71784ced0a178abaa460f1ad5e8a391bdfb
Al-741 <miermona@gmail.com>
2023-03-02 10:41:33 +0100

    navbar&pages app web

---
+++ app-web/src/pages/Performance.js
@@ -0,0 +1,15 @@
+export default function Performance() {
+    return 
+      <div className="perfo"> 
+        <div> 
+          test
+          trze
+          tzrt
+          zrgtzsr
+        </div>
+        <div> 
+          tes 2 2
+        </div>
+      </div>
+    
+  }

358802fb4fc93f6de90f5c833129a9308fb1e266
Nafbo <victor.bonnaf@gmail.com>
2023-03-03 16:47:41 +0100

    a

--- src/app/LiveBot/BotTrading.py
+++ src/app/LiveBot/BotTrading.py
@@ -402,7 +402,7 @@ def BotTrading(pairs, apiKey, secret, password, id, running, maxActivePositions)
                     print(e)
                 
             if trade.openShortPosition(actualRow, previousRow) and positionInProgress[compte] == '' and usd_balance>1 and running == True: 
-                f2 = open("src/app/LiveBot/TradeMax.json")
+                f2 = open("src/app/LiveBot/TradeMax.json") #Ajouter AVA-Bot/ pour unbuntu
                 TradeMax = json.load(f2)
                 f2.close()
                 try: 

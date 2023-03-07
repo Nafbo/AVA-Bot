19a01c29a00eec4e058db72221777406689fd489
Nafbo <victor.bonnaf@gmail.com>
2023-03-07 20:07:19 +0100

    a

--- app-web/node_modules/tsparticles-interaction-particles-collisions/esm/Bounce.js
+++
@@ -1,4 +0,0 @@
-import { circleBounce, circleBounceDataFromParticle } from "tsparticles-engine";
-export function bounce(p1, p2) {
-    circleBounce(circleBounceDataFromParticle(p1), circleBounceDataFromParticle(p2));
-}
--- app-web/node_modules/tsparticles-shape-square/browser/SquareDrawer.js
+++
@@ -1,9 +0,0 @@
-const fixFactor = Math.sqrt(2);
-export class SquareDrawer {
-    draw(context, particle, radius) {
-        context.rect(-radius / fixFactor, -radius / fixFactor, (radius * 2) / fixFactor, (radius * 2) / fixFactor);
-    }
-    getSidesCount() {
-        return 4;
-    }
-}

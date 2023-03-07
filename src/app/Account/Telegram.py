0b9661da3b03c651630e717fda9344650ca0132b
Nafbo <victor.bonnaf@gmail.com>
2023-03-07 13:26:20 +0100

    a

---
+++ src/app/Account/Telegram.py
@@ -0,0 +1,10 @@
+import requests
+TOKEN = '6091261455:AAEQ6ZZlLqkLYyNOOZkDpujQJ3dyLjAfzj8'
+chat_id = "6210537537"
+message = "Valoche est a la ramasse"
+url = "https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}".format(TOKEN, chat_id, message)
+
+print(requests.get(url).json())
+
+# url = "https://api.telegram.org/bot{}/getUpdates".format(TOKEN)
+# print(requests.get(url).json())
